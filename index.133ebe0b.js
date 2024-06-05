4c6ef8ad4359320ee1f52ce0bf8df3694bdbc706
Oksana Pokhvalenko <oksanapokhvalenko@gmail.com>
2024-06-05 18:01:54 +0200

    make build

---
+++ index.133ebe0b.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelector(".gallery__list").addEventListener("click",function(r){r.preventDefault();var t=r.target.closest("a");e.src=t.href});
+//# sourceMappingURL=index.133ebe0b.js.map
